1ce53df9b48bcd2eb62e280ccac514398726824d
Dominik B <dominik.boecker@sn-invent.de>
2026-01-19 15:09:33 +0100

    Aufgabe 2.2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,18 @@
 import { Component, signal } from '@angular/core';
+import { Product } from './product/product';
 
 @Component({
   selector: 'stn-root',
   templateUrl: './app.html',
   standalone: false,
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('stones');
+
+  productParent = new Product(0, 'Grabstein Gravo', 12.56, 32.45);
+
+  onPriceChange(price: number) {
+    alert('Neuer Preis: ' + price);
+  }
 }
--- src/app/product/product-display/product-display.ts
+++ src/app/product/product-display/product-display.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../product';
 
 @Component({
@@ -8,10 +8,11 @@ import { Product } from '../product';
   styleUrl: './product-display.css',
 })
 export class ProductDisplay {
-  product: Product = new Product(0, 'Grabstein Gravo', 12.56, 32.45);
+  @Input({ required: true }) product!: Product;
+  @Output() priceChange = new EventEmitter<number>();
 
   increasePrice() {
     this.product.price += 5;
-    alert('Neuer Preis: ' + this.product.price);
+    this.priceChange.emit(this.product.price);
   }
 }
