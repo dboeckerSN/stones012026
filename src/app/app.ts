68bc23191ea10980679850aa5db4cca554274dc5
Dominik B <dominik.boecker@sn-invent.de>
2026-01-21 10:47:22 +0100

    Aufgabe 7.1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { Product } from './product/product';
+
 
 @Component({
   selector: 'stn-root',
@@ -10,56 +10,8 @@ import { Product } from './product/product';
 export class App {
   protected readonly title = signal('stones');
   heute = new Date();
-  products: Product[] = [
-    {
-      id: 0,
-      name: 'nisi elit',
-      price: 85.59,
-      weight: 22,
-    },
-    {
-      id: 1,
-      name: 'pariatur aliquip',
-      price: 122.39,
-      weight: 21,
-    },
-    {
-      id: 2,
-      name: 'nulla cillum',
-      price: 151.49,
-      weight: 20,
-    },
-    {
-      id: 3,
-      name: 'id exercitation',
-      price: 128.45,
-      weight: 23,
-    },
-    {
-      id: 4,
-      name: 'commodo minim',
-      price: 111.34,
-      weight: 21,
-    },
-    {
-      id: 5,
-      name: 'duis tempor',
-      price: 164.61,
-      weight: 24,
-    },
-    {
-      id: 6,
-      name: 'excepteur nostrud',
-      price: 71.62,
-      weight: 38,
-    },
-  ];
 
   onPriceChange(price: number) {
     alert('Neuer Preis: ' + price);
-  }
-
-  onSaveProduct(product: Product) {
-    this.products.unshift(product);
   }
 }
---
+++ src/app/product/product-list/product-list.spec.ts
@@ -0,0 +1,20 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProductList } from './product-list';
+
+describe('ProductList', () => {
+  let component: ProductList;
+  let fixture: ComponentFixture<ProductList>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({}).compileComponents();
+
+    fixture = TestBed.createComponent(ProductList);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
